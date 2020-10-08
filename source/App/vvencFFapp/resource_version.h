218810501c35961038d2bb3ced57baa9fcd151bf
Jens Güther <jens.guether@hhi.fraunhofer.de>
2020-10-08 12:54:25 +0200

    added rc support for apps

---
+++ source/App/vvencFFapp/resource_version.h
@@ -0,0 +1,10 @@
+#if !defined( resource_version_h )
+#  define resource_version_h
+
+// pick up top level version information.
+#  include "vvenc/version.h"
+
+#  define VS_FILE_VERSION VVENC_VS_VERSION
+#  define VS_FILE_VERSION_STR VVENC_VS_VERSION_STR
+
+#endif
