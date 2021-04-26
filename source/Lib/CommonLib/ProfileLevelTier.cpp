9ff9f6a0c34682f6c2ed1aaef393f496b545d6f2
Christian Lehmann <christian.lehmann@hhi.fraunhofer.de>
2021-04-26 11:28:15 +0200

    auto-level auto detect limit max bitrate to level limie 6.3 (320mbit)

--- source/Lib/CommonLib/ProfileLevelTier.cpp
+++ source/Lib/CommonLib/ProfileLevelTier.cpp
@@ -125,7 +125,8 @@ const ProfileFeatures *ProfileFeatures::getProfileFeatures(const vvencProfile p)
 vvencLevel LevelTierFeatures::getLevelForInput( uint32_t width, uint32_t height, bool tier, int temporalRate, int temporalScale, int bitrate )
 {
   uint64_t samplesPerSec = ((uint64_t)temporalRate*(uint64_t)width*(uint64_t)height)/temporalScale;
-  int br = bitrate ? bitrate/1000 : samplesPerSec/3000;  //assume a min compression factor of 36 in the case of fix qp
+  uint32_t br = bitrate ? (uint32_t)bitrate/1000 : std::min((uint32_t)320000,(uint32_t)(samplesPerSec/3000));  //assume a min compression factor of 36 in the case of fix qp
+
   for (const auto& info: mainLevelTierInfo )
   {
     if ( width <= info.getMaxPicWidthInLumaSamples() &&
