e05174b98befe962c452326892983755a7ea1807
Adam Wieckowski <adam.wieckowski@hhi.fraunhofer.de>
2021-05-10 11:38:18 +0200

    removed obsolete memsets

--- source/Lib/EncoderLib/InterSearch.cpp
+++ source/Lib/EncoderLib/InterSearch.cpp
@@ -6036,9 +6036,6 @@ void InterSearch::xIntraPatternSearchIBC(CodingUnit& cu, TZSearchStruct& cStruct
     m_numBVs = xMergeCandLists(m_acBVs, m_numBVs, (2 * IBC_NUM_CANDIDATES), m_defaultCachedBvs->m_bvCands, m_defaultCachedBvs->currCnt);
 
     Mv cMvPredEncOnly[IBC_NUM_CANDIDATES];
-    GCC_WARNING_DISABLE_class_memaccess
-    memset( cMvPredEncOnly, 0, sizeof( cMvPredEncOnly ) );
-    GCC_WARNING_RESET
     int nbPreds = 0;
     CU::getIbcMVPsEncOnly(cu, cMvPredEncOnly, nbPreds);
     m_numBVs = xMergeCandLists(m_acBVs, m_numBVs, (2 * IBC_NUM_CANDIDATES), cMvPredEncOnly, nbPreds);
@@ -6367,9 +6364,6 @@ void InterSearch::xIBCEstimation(CodingUnit& cu, PelUnitBuf& origBuf, Mv* pcMvPr
     if (buffered)
     {
       Mv cMvPredEncOnly[IBC_NUM_CANDIDATES];
-      GCC_WARNING_DISABLE_class_memaccess
-      memset( cMvPredEncOnly, 0, sizeof( cMvPredEncOnly ) );
-      GCC_WARNING_RESET
       int nbPreds = 0;
       CU::getIbcMVPsEncOnly(cu, cMvPredEncOnly, nbPreds);
 
