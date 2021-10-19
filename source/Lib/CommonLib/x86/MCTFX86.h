fd6983ef18369bad3870716a9bb95b81fc857224
Adam Wieckowski <adam.wieckowski@hhi.fraunhofer.de>
2021-10-19 17:37:26 +0200

    improved the SSE41 implementation

--- source/Lib/CommonLib/x86/MCTFX86.h
+++ source/Lib/CommonLib/x86/MCTFX86.h
@@ -339,19 +339,22 @@ int motionErrorLumaFrac_SIMD( const Pel* origOrigin, const ptrdiff_t origStride,
     }
   }
 #else
-  __m128i xfilt12 = _mm_unpacklo_epi16( _mm_set1_epi16( yFilter[1] ), _mm_set1_epi16( yFilter[2] ) );
-  __m128i xfilt34 = _mm_unpacklo_epi16( _mm_set1_epi16( yFilter[3] ), _mm_set1_epi16( yFilter[4] ) );
-  __m128i xfilt56 = _mm_unpacklo_epi16( _mm_set1_epi16( yFilter[5] ), _mm_set1_epi16( yFilter[6] ) );
+  const __m128i yfilt12 = _mm_unpacklo_epi16( _mm_set1_epi16( yFilter[1] ), _mm_set1_epi16( yFilter[2] ) );
+  const __m128i yfilt34 = _mm_unpacklo_epi16( _mm_set1_epi16( yFilter[3] ), _mm_set1_epi16( yFilter[4] ) );
+  const __m128i yfilt56 = _mm_unpacklo_epi16( _mm_set1_epi16( yFilter[5] ), _mm_set1_epi16( yFilter[6] ) );
+
+  const __m128i xfilt12 = _mm_unpacklo_epi16( _mm_set1_epi16( xFilter[1] ), _mm_set1_epi16( xFilter[2] ) );
+  const __m128i xfilt34 = _mm_unpacklo_epi16( _mm_set1_epi16( xFilter[3] ), _mm_set1_epi16( xFilter[4] ) );
+  const __m128i xfilt56 = _mm_unpacklo_epi16( _mm_set1_epi16( xFilter[5] ), _mm_set1_epi16( xFilter[6] ) );
   
-  __m128i xfilt1 = _mm_loadu_si128( ( const __m128i * ) xFilter );
-  __m128i xmax   = _mm_set1_epi32( maxSampleValue );
-  __m128i xmin   = _mm_setzero_si128();
+  const __m128i xmax   = _mm_set1_epi16( maxSampleValue );
+  const __m128i xmin   = _mm_setzero_si128();
   
   const int yOffset    = y + 1 + ( dy >> 4 ) - 3;
   const Pel* sourceCol = buffOrigin + base + yOffset * buffStride;
   const Pel* origCol   = origOrigin + y * origStride + x;
 
-  for( int x1 = 0; x1 < bs; x1 += 4, sourceCol += 4, origCol += 4 )
+  for( int x1 = 0; x1 < bs; x1 += 8, sourceCol += 8, origCol += 8 )
   {
     const Pel* origRow  = origCol;
     const Pel* rowStart = sourceCol;
@@ -360,26 +363,32 @@ int motionErrorLumaFrac_SIMD( const Pel* origOrigin, const ptrdiff_t origStride,
 
     for( int y1 = 1; y1 < bs + 6; y1++, rowStart += buffStride )
     {
-      __m128i xsrc0 = _mm_loadu_si128( ( const __m128i * ) &rowStart[0] );
       __m128i xsrc1 = _mm_loadu_si128( ( const __m128i * ) &rowStart[1] );
       __m128i xsrc2 = _mm_loadu_si128( ( const __m128i * ) &rowStart[2] );
       __m128i xsrc3 = _mm_loadu_si128( ( const __m128i * ) &rowStart[3] );
-
-      xsrc0 = _mm_madd_epi16( xsrc0, xfilt1 );
-      xsrc1 = _mm_madd_epi16( xsrc1, xfilt1 );
-      xsrc2 = _mm_madd_epi16( xsrc2, xfilt1 );
-      xsrc3 = _mm_madd_epi16( xsrc3, xfilt1 );
-
-      xsrc0 = _mm_hadd_epi32( xsrc0, xsrc1 );
-      xsrc2 = _mm_hadd_epi32( xsrc2, xsrc3 );
-
-      xsrc0 = _mm_hadd_epi32( xsrc0, xsrc2 );
+      __m128i xsrc4 = _mm_loadu_si128( ( const __m128i * ) &rowStart[4] );
+      __m128i xsrc5 = _mm_loadu_si128( ( const __m128i * ) &rowStart[5] );
+      __m128i xsrc6 = _mm_loadu_si128( ( const __m128i * ) &rowStart[6] );
 
       __m128i
-      xsum = _mm_add_epi32  ( xsrc0, _mm_set1_epi32( 1 << 5 ) );
-      xsum = _mm_srai_epi32 ( xsum,  6 );
-      xsum = _mm_min_epi32  ( xmax,  _mm_max_epi32( xmin, xsum ) );
-      xsum = _mm_packs_epi32( xsum,  _mm_setzero_si128() );
+      xsum0 = _mm_set1_epi32( 1 << 5 );
+      __m128i
+      xsum1 = _mm_set1_epi32( 1 << 5 );
+
+      xsum0 = _mm_add_epi32( xsum0, _mm_madd_epi16( _mm_unpacklo_epi16( xsrc1, xsrc2 ), xfilt12 ) );
+      xsum1 = _mm_add_epi32( xsum1, _mm_madd_epi16( _mm_unpackhi_epi16( xsrc1, xsrc2 ), xfilt12 ) );
+
+      xsum0 = _mm_add_epi32( xsum0, _mm_madd_epi16( _mm_unpacklo_epi16( xsrc3, xsrc4 ), xfilt34 ) );
+      xsum1 = _mm_add_epi32( xsum1, _mm_madd_epi16( _mm_unpackhi_epi16( xsrc3, xsrc4 ), xfilt34 ) );
+
+      xsum0 = _mm_add_epi32( xsum0, _mm_madd_epi16( _mm_unpacklo_epi16( xsrc5, xsrc6 ), xfilt56 ) );
+      xsum1 = _mm_add_epi32( xsum1, _mm_madd_epi16( _mm_unpackhi_epi16( xsrc5, xsrc6 ), xfilt56 ) );
+
+      xsum0 = _mm_srai_epi32( xsum0, 6 );
+      xsum1 = _mm_srai_epi32( xsum1, 6 );
+      __m128i
+      xsum  = _mm_packs_epi32( xsum0, xsum1 );
+      xsum  = _mm_min_epi16( xmax, _mm_max_epi16( xmin, xsum ) );
 
       if( y1 >= 6 )
       {
@@ -390,22 +399,31 @@ int motionErrorLumaFrac_SIMD( const Pel* origOrigin, const ptrdiff_t origStride,
         xsrc[4] = xsrc[5];
         xsrc[5] = xsum;
         
-        xsum = _mm_set1_epi32( 1 << 5 );
+        xsum0 = _mm_set1_epi32( 1 << 5 );
+        xsum1 = _mm_set1_epi32( 1 << 5 );
 
-        xsum = _mm_add_epi32( xsum, _mm_madd_epi16( xfilt12, _mm_unpacklo_epi16( xsrc[0], xsrc[1] ) ) );
-        xsum = _mm_add_epi32( xsum, _mm_madd_epi16( xfilt34, _mm_unpacklo_epi16( xsrc[2], xsrc[3] ) ) );
-        xsum = _mm_add_epi32( xsum, _mm_madd_epi16( xfilt56, _mm_unpacklo_epi16( xsrc[4], xsrc[5] ) ) );
+        xsum0 = _mm_add_epi32( xsum0, _mm_madd_epi16( yfilt12, _mm_unpacklo_epi16( xsrc[0], xsrc[1] ) ) );
+        xsum1 = _mm_add_epi32( xsum1, _mm_madd_epi16( yfilt12, _mm_unpackhi_epi16( xsrc[0], xsrc[1] ) ) );
+
+        xsum0 = _mm_add_epi32( xsum0, _mm_madd_epi16( yfilt34, _mm_unpacklo_epi16( xsrc[2], xsrc[3] ) ) );
+        xsum1 = _mm_add_epi32( xsum1, _mm_madd_epi16( yfilt34, _mm_unpackhi_epi16( xsrc[2], xsrc[3] ) ) );
+
+        xsum0 = _mm_add_epi32( xsum0, _mm_madd_epi16( yfilt56, _mm_unpacklo_epi16( xsrc[4], xsrc[5] ) ) );
+        xsum1 = _mm_add_epi32( xsum1, _mm_madd_epi16( yfilt56, _mm_unpackhi_epi16( xsrc[4], xsrc[5] ) ) );
         
-        xsum = _mm_srai_epi32 ( xsum, 6 );
-        xsum = _mm_min_epi32  ( xmax, _mm_max_epi32( xmin, xsum ) );
-        xsum = _mm_packs_epi32( xsum, _mm_setzero_si128() );
+        xsum0 = _mm_srai_epi32( xsum0, 6 );
+        xsum1 = _mm_srai_epi32( xsum1, 6 );
+
+        xsum  = _mm_packs_epi32( xsum0, xsum1 );
+        xsum  = _mm_min_epi16  ( xmax, _mm_max_epi16( xmin, xsum ) );
 
         __m128i
-        xorg = _mm_loadl_epi64( ( const __m128i * ) origRow );
+        xorg = _mm_loadu_si128( ( const __m128i * ) origRow );
         origRow += origStride;
 
         xsum = _mm_sub_epi16 ( xsum, xorg );
         xsum = _mm_madd_epi16( xsum, xsum );
+        xsum = _mm_hadd_epi32( xsum, xsum );
 
         error += _mm_extract_epi32( xsum, 0 );
         error += _mm_extract_epi32( xsum, 1 );
