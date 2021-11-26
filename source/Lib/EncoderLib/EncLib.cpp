dcedb02d7da345a8a354e32148e4ef4845215896
Jens Brandenburg <jens.brandenburg@hhi.fraunhofer.de>
2021-11-26 10:21:36 +0100

    fix previously broken fix QP encoding

--- source/Lib/EncoderLib/EncLib.cpp
+++ source/Lib/EncoderLib/EncLib.cpp
@@ -232,14 +232,8 @@ void EncLib::initPass( int pass, const char* statsFName )
 
   // gop encoder
   m_gopEncoder = new EncGOP();
-  if ( m_encCfg.m_RCLookAhead )
-  {
-    m_gopEncoder->initStage( m_encCfg.m_IntraPeriod /*+ m_firstPassCfg.m_GOPSize*/ + 1, false, false, m_encCfg.m_CTUSize );
-  }
-  else
-  {
-    m_gopEncoder->initStage( m_firstPassCfg.m_GOPSize + 1, false, false, m_encCfg.m_CTUSize );
-  }
+  const int encDelay = m_encCfg.m_RCLookAhead ? m_encCfg.m_IntraPeriod + 1 : m_encCfg.m_GOPSize + 1;
+  m_gopEncoder->initStage( encDelay, false, false, m_encCfg.m_CTUSize );
   m_gopEncoder->init( m_encCfg, *m_rateCtrl, m_threadPool, false );
   if( m_rateCtrl->rcIsFinalPass )
   {
