8d409194ed5a8ca1300eab292f3374da7e43ff57
Adam Wieckowski <adam.wieckowski@hhi.fraunhofer.de>
2022-01-19 10:06:48 +0100

    added copyright update to -2022

--- source/Lib/CommonLib/PicYuvMD5.cpp
+++ source/Lib/CommonLib/PicYuvMD5.cpp
@@ -14,7 +14,7 @@ Einsteinufer 37
 www.hhi.fraunhofer.de/vvc
 vvc@hhi.fraunhofer.de
 
-Copyright (c) 2019-2021, Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+Copyright (c) 2019-2022, Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
