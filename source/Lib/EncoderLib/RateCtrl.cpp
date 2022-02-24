136c093aa07c026d41766258b1ff5b9b6f5baaf2
Christian Helmrich <christian.helmrich@hhi.fraunhofer.de>
2022-02-24 15:10:59 +0000

    Correct precision and add low-QP correction in second-pass base QP estimation for lookahead RC

--- source/Lib/EncoderLib/RateCtrl.cpp
+++ source/Lib/EncoderLib/RateCtrl.cpp
@@ -368,14 +368,15 @@ int RateCtrl::getBaseQP()
 {
   // estimate near-optimal base QP for PPS in second RC pass
   double d = (3840.0 * 2160.0) / double (m_pcEncCfg->m_SourceWidth * m_pcEncCfg->m_SourceHeight);
-  const int firstPassBaseQP = std::max (17, MAX_QP_PERCEPT_QPA - 2 - int (0.5 + sqrt ((d * m_pcEncCfg->m_RCTargetBitrate) / 500000.0)));
+  const double firstQPOffset = sqrt ((d * m_pcEncCfg->m_RCTargetBitrate) / 500000.0);
+  const int log2HeightMinus7 = int (0.5 + log ((double) std::max (128, m_pcEncCfg->m_SourceHeight)) / log (2.0)) - 7;
   const unsigned fps = m_pcEncCfg->m_FrameRate / m_pcEncCfg->m_FrameScale;
   std::list<TRCPassStats>& firstPassData = m_listRCFirstPassStats;
   int baseQP = MAX_QP;
 
   if (firstPassData.size() > 0 && fps > 0)
   {
-    const int log2HeightMinus7 = int (0.5 + log ((double) std::max (128, m_pcEncCfg->m_SourceHeight)) / log (2.0)) - 7;
+    const int firstPassBaseQP = std::max (17, MAX_QP_PERCEPT_QPA - 2 - int (0.5 + firstQPOffset));
     uint64_t sumFrBits = 0, sumVisAct = 0; // first-pass data
 
     for (auto& stats : firstPassData)
@@ -389,7 +390,8 @@ int RateCtrl::getBaseQP()
   }
   else if (m_pcEncCfg->m_LookAhead)
   {
-    baseQP = firstPassBaseQP - (std::max (0, MAX_QP_PERCEPT_QPA - firstPassBaseQP) >> 1);
+    d = MAX_QP_PERCEPT_QPA - 2.0 - 1.5 * firstQPOffset;
+    baseQP = int (0.5 + d + 0.125 * log2HeightMinus7 * std::max (0.0, 24.0 - d));
   }
 
   return Clip3 (17, MAX_QP, baseQP);
