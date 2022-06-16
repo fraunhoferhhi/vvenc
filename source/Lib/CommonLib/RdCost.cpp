919f3c677b1f2ffe3e12941512fbc028a9aa619b
Adam Wieckowski <adam.wieckowski@hhi.fraunhofer.de>
2022-06-16 14:23:54 +0200

    revert early exit, not bit equal

--- source/Lib/CommonLib/RdCost.cpp
+++ source/Lib/CommonLib/RdCost.cpp
@@ -1864,11 +1864,6 @@ Distortion RdCost::xGetHADs( const DistParam &rcDtParam )
       }
       piOrg += iStrideOrg * 8;
       piCur += iStrideCur * 8;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( iCols < iRows && ( iCols & 7 ) == 0 && ( iRows & 15 ) == 0 )
@@ -1881,11 +1876,6 @@ Distortion RdCost::xGetHADs( const DistParam &rcDtParam )
       }
       piOrg += iStrideOrg * 16;
       piCur += iStrideCur * 16;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( iCols > iRows && ( iRows & 3 ) == 0 && ( iCols & 7 ) == 0 )
@@ -1922,11 +1912,6 @@ Distortion RdCost::xGetHADs( const DistParam &rcDtParam )
       }
       piOrg += 16 * iStrideOrg;
       piCur += 16 * iStrideCur;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( ( iRows % 8 == 0 ) && ( iCols % 8 == 0 ) )
@@ -1939,11 +1924,6 @@ Distortion RdCost::xGetHADs( const DistParam &rcDtParam )
       }
       piOrg += 8*iStrideOrg;
       piCur += 8*iStrideCur;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( ( iRows % 4 == 0 ) && ( iCols % 4 == 0 ) )
--- source/Lib/CommonLib/x86/RdCostX86.h
+++ source/Lib/CommonLib/x86/RdCostX86.h
@@ -2745,11 +2745,6 @@ Distortion RdCost::xGetHADs_SIMD( const DistParam &rcDtParam )
       }
       piOrg += 8*iStrideOrg;
       piCur += 8*iStrideCur;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( iCols < iRows && ( iRows & 15 ) == 0 && ( iCols & 7 ) == 0 )
@@ -2765,11 +2760,6 @@ Distortion RdCost::xGetHADs_SIMD( const DistParam &rcDtParam )
       }
       piOrg += 16*iStrideOrg;
       piCur += 16*iStrideCur;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( iCols > iRows && ( iCols & 7 ) == 0 && ( iRows & 3 ) == 0 )
@@ -2806,11 +2796,6 @@ Distortion RdCost::xGetHADs_SIMD( const DistParam &rcDtParam )
       }
       piOrg += 32 * iStrideOrg;
       piCur += 32 * iStrideCur;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( fastHad && ( ( ( iRows | iCols ) & 31 ) == 0 ) && ( iRows == iCols ) )
@@ -2823,11 +2808,6 @@ Distortion RdCost::xGetHADs_SIMD( const DistParam &rcDtParam )
       }
       piOrg += 16 * iStrideOrg;
       piCur += 16 * iStrideCur;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( vext >= AVX2 && ( ( ( iRows | iCols ) & 15 ) == 0 ) && ( iRows == iCols ) )
@@ -2840,11 +2820,6 @@ Distortion RdCost::xGetHADs_SIMD( const DistParam &rcDtParam )
       }
       piOrg += 16*iStrideOrg;
       piCur += 16*iStrideCur;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( ( ( ( iRows | iCols ) & 7 ) == 0 ) && ( iRows == iCols ) )
@@ -2857,11 +2832,6 @@ Distortion RdCost::xGetHADs_SIMD( const DistParam &rcDtParam )
       }
       piOrg += 8*iStrideOrg;
       piCur += 8*iStrideCur;
-
-      if( ( uiSum >> DISTORTION_PRECISION_ADJUSTMENT( rcDtParam.bitDepth ) ) > rcDtParam.maximumDistortionForEarlyExit )
-      {
-        break;
-      }
     }
   }
   else if( ( iRows % 4 == 0 ) && ( iCols % 4 == 0 ) )
