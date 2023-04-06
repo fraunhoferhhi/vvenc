1eb716f37cdc5ff161c6627ec0bff1da3e0371f7
Christian Bartnik <christian.bartnik@hhi.fraunhofer.de>
2023-04-06 17:57:23 +0200

    log picture hash without sei message

--- source/Lib/CommonLib/Buffer.h
+++ source/Lib/CommonLib/Buffer.h
@@ -172,6 +172,7 @@ struct AreaBuf : public Size
   void rspSignal            ( const AreaBuf<const T>& other, const Pel* pLUT );
   void scaleSignal          ( const int scale, const bool dir , const ClpRng& clpRng);
   bool compare              ( const AreaBuf<const T>& other ) const;
+
         T& at( const int& x, const int& y )          { return buf[y * stride + x]; }
   const T& at( const int& x, const int& y ) const    { return buf[y * stride + x]; }
 
