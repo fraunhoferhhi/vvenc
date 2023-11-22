632dea7a0c3d604552507ec6ea089f86a8a65849
Adam Wieckowski <70575289+adamjw24@users.noreply.github.com>
2023-11-22 09:27:43 +0100

    Workaround for MSVC 17.8 bug (#333)
    * Workaround for MSVC 17.8 bug
    
    fixes #330
    
    * Bugfix to the bugfix

--- source/Lib/CommonLib/x86/InitX86.cpp
+++ source/Lib/CommonLib/x86/InitX86.cpp
@@ -147,11 +147,14 @@ void LoopFilter::initLoopFilterX86()
 void RdCost::initRdCostX86()
 {
   auto vext = read_x86_extension_flags();
-  switch (vext){
+  switch (vext){ 
     case AVX512:
     case AVX2:
+#if defined( _MSC_VER ) && _MSC_VER >= 1938
+#else
       _initRdCostX86<AVX2>();
       break;
+#endif
     case AVX:
       _initRdCostX86<AVX>();
       break;
