ea3c7b98ad23fe089477951edf89859af9fe3c8d
Alex Davicenko <alex.davicenko@arm.com>
2026-04-10 11:37:24 +0100

    Arm: Add SVE2 implementation of xCalcHAD16x8_sve2
    Add SVE2 implementation of xCalcHAD16x8_sve2. This provides up to 7%
    uplift compared to Neon when measured on a Neoverse V2 machine with
    LLVM-21.

--- source/Lib/CommonLib/arm/sve2/RdCost_sve2.cpp
+++ source/Lib/CommonLib/arm/sve2/RdCost_sve2.cpp
@@ -506,6 +506,208 @@ Distortion xCalcHAD8x16_sve2( const Pel* piOrg, const Pel* piCur, const ptrdiff_
   return sad;
 }
 
+Distortion xCalcHAD16x8_sve2( const Pel* piOrg, const Pel* piCur, const ptrdiff_t iStrideOrg,
+                              const ptrdiff_t iStrideCur )
+{
+  int16x8_t m1[16], m2[16];
+  int32x4_t m3[32];
+
+  for( int k = 0; k < 8; k++ )
+  {
+    int16x8_t r0_lo = vld1q_s16( piOrg + k * iStrideOrg );
+    int16x8_t r0_hi = vld1q_s16( piOrg + k * iStrideOrg + 8 );
+    int16x8_t r1_lo = vld1q_s16( piCur + k * iStrideCur );
+    int16x8_t r1_hi = vld1q_s16( piCur + k * iStrideCur + 8 );
+
+    m2[2 * k + 0] = vsubq_s16( r0_lo, r1_lo );
+    m2[2 * k + 1] = vsubq_s16( r0_hi, r1_hi ); // 11-bit
+  }
+
+  const uint8x16_t idx0 = vld1q_u8( kTrnSwapTbl0 );
+  const uint8x16_t idx1 = vld1q_u8( kTrnSwapTbl1 );
+
+  // Vertical distance 4.
+  m1[0] = vaddq_s16( m2[0], m2[8] );
+  m1[1] = vaddq_s16( m2[1], m2[9] );
+  m1[2] = vaddq_s16( m2[2], m2[10] );
+  m1[3] = vaddq_s16( m2[3], m2[11] );
+  m1[4] = vaddq_s16( m2[4], m2[12] );
+  m1[5] = vaddq_s16( m2[5], m2[13] );
+  m1[6] = vaddq_s16( m2[6], m2[14] );
+  m1[7] = vaddq_s16( m2[7], m2[15] );
+  m1[8] = vsubq_s16( m2[0], m2[8] );
+  m1[9] = vsubq_s16( m2[1], m2[9] );
+  m1[10] = vsubq_s16( m2[2], m2[10] );
+  m1[11] = vsubq_s16( m2[3], m2[11] );
+  m1[12] = vsubq_s16( m2[4], m2[12] );
+  m1[13] = vsubq_s16( m2[5], m2[13] );
+  m1[14] = vsubq_s16( m2[6], m2[14] );
+  m1[15] = vsubq_s16( m2[7], m2[15] ); // 12-bit
+
+  // Vertical distance 2.
+  m2[0] = vaddq_s16( m1[0], m1[4] );
+  m2[1] = vaddq_s16( m1[1], m1[5] );
+  m2[2] = vaddq_s16( m1[2], m1[6] );
+  m2[3] = vaddq_s16( m1[3], m1[7] );
+  m2[4] = vsubq_s16( m1[0], m1[4] );
+  m2[5] = vsubq_s16( m1[1], m1[5] );
+  m2[6] = vsubq_s16( m1[2], m1[6] );
+  m2[7] = vsubq_s16( m1[3], m1[7] );
+  m2[8] = vaddq_s16( m1[8], m1[12] );
+  m2[9] = vaddq_s16( m1[9], m1[13] );
+  m2[10] = vaddq_s16( m1[10], m1[14] );
+  m2[11] = vaddq_s16( m1[11], m1[15] );
+  m2[12] = vsubq_s16( m1[8], m1[12] );
+  m2[13] = vsubq_s16( m1[9], m1[13] );
+  m2[14] = vsubq_s16( m1[10], m1[14] );
+  m2[15] = vsubq_s16( m1[11], m1[15] ); // 13-bit
+
+  // Vertical distance 1.
+  m1[0] = vaddq_s16( m2[0], m2[2] );
+  m1[1] = vaddq_s16( m2[1], m2[3] );
+  m1[2] = vsubq_s16( m2[0], m2[2] );
+  m1[3] = vsubq_s16( m2[1], m2[3] );
+  m1[4] = vaddq_s16( m2[4], m2[6] );
+  m1[5] = vaddq_s16( m2[5], m2[7] );
+  m1[6] = vsubq_s16( m2[4], m2[6] );
+  m1[7] = vsubq_s16( m2[5], m2[7] );
+  m1[8] = vaddq_s16( m2[8], m2[10] );
+  m1[9] = vaddq_s16( m2[9], m2[11] );
+  m1[10] = vsubq_s16( m2[8], m2[10] );
+  m1[11] = vsubq_s16( m2[9], m2[11] );
+  m1[12] = vaddq_s16( m2[12], m2[14] );
+  m1[13] = vaddq_s16( m2[13], m2[15] );
+  m1[14] = vsubq_s16( m2[12], m2[14] );
+  m1[15] = vsubq_s16( m2[13], m2[15] ); // 14-bit
+
+  // Horizontal distance 1.
+  m2[0] = vvenc_cadd_s16<90>( m1[0], m1[0] );
+  m2[1] = vvenc_cadd_s16<90>( m1[1], m1[1] );
+  m2[2] = vvenc_cadd_s16<90>( m1[2], m1[2] );
+  m2[3] = vvenc_cadd_s16<90>( m1[3], m1[3] );
+  m2[4] = vvenc_cadd_s16<90>( m1[4], m1[4] );
+  m2[5] = vvenc_cadd_s16<90>( m1[5], m1[5] );
+  m2[6] = vvenc_cadd_s16<90>( m1[6], m1[6] );
+  m2[7] = vvenc_cadd_s16<90>( m1[7], m1[7] );
+  m2[8] = vvenc_cadd_s16<90>( m1[8], m1[8] );
+  m2[9] = vvenc_cadd_s16<90>( m1[9], m1[9] );
+  m2[10] = vvenc_cadd_s16<90>( m1[10], m1[10] );
+  m2[11] = vvenc_cadd_s16<90>( m1[11], m1[11] );
+  m2[12] = vvenc_cadd_s16<90>( m1[12], m1[12] );
+  m2[13] = vvenc_cadd_s16<90>( m1[13], m1[13] );
+  m2[14] = vvenc_cadd_s16<90>( m1[14], m1[14] );
+  m2[15] = vvenc_cadd_s16<90>( m1[15], m1[15] ); // 15-bit
+
+  m1[0] = vvenc_vqtbl2q_s16_u8( m2[0], m2[1], idx0 );
+  m1[1] = vvenc_vqtbl2q_s16_u8( m2[0], m2[1], idx1 );
+  m1[2] = vvenc_vqtbl2q_s16_u8( m2[2], m2[3], idx0 );
+  m1[3] = vvenc_vqtbl2q_s16_u8( m2[2], m2[3], idx1 );
+  m1[4] = vvenc_vqtbl2q_s16_u8( m2[4], m2[5], idx0 );
+  m1[5] = vvenc_vqtbl2q_s16_u8( m2[4], m2[5], idx1 );
+  m1[6] = vvenc_vqtbl2q_s16_u8( m2[6], m2[7], idx0 );
+  m1[7] = vvenc_vqtbl2q_s16_u8( m2[6], m2[7], idx1 );
+  m1[8] = vvenc_vqtbl2q_s16_u8( m2[8], m2[9], idx0 );
+  m1[9] = vvenc_vqtbl2q_s16_u8( m2[8], m2[9], idx1 );
+  m1[10] = vvenc_vqtbl2q_s16_u8( m2[10], m2[11], idx0 );
+  m1[11] = vvenc_vqtbl2q_s16_u8( m2[10], m2[11], idx1 );
+  m1[12] = vvenc_vqtbl2q_s16_u8( m2[12], m2[13], idx0 );
+  m1[13] = vvenc_vqtbl2q_s16_u8( m2[12], m2[13], idx1 );
+  m1[14] = vvenc_vqtbl2q_s16_u8( m2[14], m2[15], idx0 );
+  m1[15] = vvenc_vqtbl2q_s16_u8( m2[14], m2[15], idx1 );
+
+  // Horizontal distance 2.
+  m2[0] = vvenc_cadd_s16<90>( m1[0], m1[0] );
+  m2[1] = vvenc_cadd_s16<90>( m1[1], m1[1] );
+  m2[2] = vvenc_cadd_s16<90>( m1[2], m1[2] );
+  m2[3] = vvenc_cadd_s16<90>( m1[3], m1[3] );
+  m2[4] = vvenc_cadd_s16<90>( m1[4], m1[4] );
+  m2[5] = vvenc_cadd_s16<90>( m1[5], m1[5] );
+  m2[6] = vvenc_cadd_s16<90>( m1[6], m1[6] );
+  m2[7] = vvenc_cadd_s16<90>( m1[7], m1[7] );
+  m2[8] = vvenc_cadd_s16<90>( m1[8], m1[8] );
+  m2[9] = vvenc_cadd_s16<90>( m1[9], m1[9] );
+  m2[10] = vvenc_cadd_s16<90>( m1[10], m1[10] );
+  m2[11] = vvenc_cadd_s16<90>( m1[11], m1[11] );
+  m2[12] = vvenc_cadd_s16<90>( m1[12], m1[12] );
+  m2[13] = vvenc_cadd_s16<90>( m1[13], m1[13] );
+  m2[14] = vvenc_cadd_s16<90>( m1[14], m1[14] );
+  m2[15] = vvenc_cadd_s16<90>( m1[15], m1[15] ); // 16-bit
+
+  // Horizontal distance 4
+  auto dcVec0 = vaddl_s16( vget_low_s16( m2[0] ), vget_low_s16( m2[1] ) );
+  auto dcVec1 = vaddl_s16( vget_high_s16( m2[0] ), vget_high_s16( m2[1] ) );
+
+  // The last butterfly uses |x+y|+|x-y| = 2*max(|x|,|y|); we delay the "*2".
+  m3[0] = vabsq_s32( vaddl_s16( vget_low_s16( m2[0] ), vget_low_s16( m2[1] ) ) );
+  m3[1] = vabdl_s16( vget_low_s16( m2[0] ), vget_low_s16( m2[1] ) );
+  m3[2] = vabsq_s32( vaddl_s16( vget_high_s16( m2[0] ), vget_high_s16( m2[1] ) ) );
+  m3[3] = vabdl_s16( vget_high_s16( m2[0] ), vget_high_s16( m2[1] ) );
+  m3[4] = vabsq_s32( vaddl_s16( vget_low_s16( m2[2] ), vget_low_s16( m2[3] ) ) );
+  m3[5] = vabdl_s16( vget_low_s16( m2[2] ), vget_low_s16( m2[3] ) );
+  m3[6] = vabsq_s32( vaddl_s16( vget_high_s16( m2[2] ), vget_high_s16( m2[3] ) ) );
+  m3[7] = vabdl_s16( vget_high_s16( m2[2] ), vget_high_s16( m2[3] ) );
+  m3[8] = vabsq_s32( vaddl_s16( vget_low_s16( m2[4] ), vget_low_s16( m2[5] ) ) );
+  m3[9] = vabdl_s16( vget_low_s16( m2[4] ), vget_low_s16( m2[5] ) );
+  m3[10] = vabsq_s32( vaddl_s16( vget_high_s16( m2[4] ), vget_high_s16( m2[5] ) ) );
+  m3[11] = vabdl_s16( vget_high_s16( m2[4] ), vget_high_s16( m2[5] ) );
+  m3[12] = vabsq_s32( vaddl_s16( vget_low_s16( m2[6] ), vget_low_s16( m2[7] ) ) );
+  m3[13] = vabdl_s16( vget_low_s16( m2[6] ), vget_low_s16( m2[7] ) );
+  m3[14] = vabsq_s32( vaddl_s16( vget_high_s16( m2[6] ), vget_high_s16( m2[7] ) ) );
+  m3[15] = vabdl_s16( vget_high_s16( m2[6] ), vget_high_s16( m2[7] ) );
+
+  int32_t absDC = vgetq_lane_s32( vabsq_s32( vaddq_s32( dcVec0, dcVec1 ) ), 3 );
+
+  // Horizontal distance 8.
+  int32x4_t max0 = vmaxq_s32( m3[0], m3[2] );
+  int32x4_t max1 = vmaxq_s32( m3[1], m3[3] );
+  int32x4_t max2 = vmaxq_s32( m3[4], m3[6] );
+  int32x4_t max3 = vmaxq_s32( m3[5], m3[7] );
+  int32x4_t max4 = vmaxq_s32( m3[8], m3[10] );
+  int32x4_t max5 = vmaxq_s32( m3[9], m3[11] );
+  int32x4_t max6 = vmaxq_s32( m3[12], m3[14] );
+  int32x4_t max7 = vmaxq_s32( m3[13], m3[15] );
+
+  m3[16] = vabsq_s32( vaddl_s16( vget_low_s16( m2[8] ), vget_low_s16( m2[9] ) ) );
+  m3[17] = vabdl_s16( vget_low_s16( m2[8] ), vget_low_s16( m2[9] ) );
+  m3[18] = vabsq_s32( vaddl_s16( vget_high_s16( m2[8] ), vget_high_s16( m2[9] ) ) );
+  m3[19] = vabdl_s16( vget_high_s16( m2[8] ), vget_high_s16( m2[9] ) );
+  m3[20] = vabsq_s32( vaddl_s16( vget_low_s16( m2[10] ), vget_low_s16( m2[11] ) ) );
+  m3[21] = vabdl_s16( vget_low_s16( m2[10] ), vget_low_s16( m2[11] ) );
+  m3[22] = vabsq_s32( vaddl_s16( vget_high_s16( m2[10] ), vget_high_s16( m2[11] ) ) );
+  m3[23] = vabdl_s16( vget_high_s16( m2[10] ), vget_high_s16( m2[11] ) );
+  m3[24] = vabsq_s32( vaddl_s16( vget_low_s16( m2[12] ), vget_low_s16( m2[13] ) ) );
+  m3[25] = vabdl_s16( vget_low_s16( m2[12] ), vget_low_s16( m2[13] ) );
+  m3[26] = vabsq_s32( vaddl_s16( vget_high_s16( m2[12] ), vget_high_s16( m2[13] ) ) );
+  m3[27] = vabdl_s16( vget_high_s16( m2[12] ), vget_high_s16( m2[13] ) );
+  m3[28] = vabsq_s32( vaddl_s16( vget_low_s16( m2[14] ), vget_low_s16( m2[15] ) ) );
+  m3[29] = vabdl_s16( vget_low_s16( m2[14] ), vget_low_s16( m2[15] ) );
+  m3[30] = vabsq_s32( vaddl_s16( vget_high_s16( m2[14] ), vget_high_s16( m2[15] ) ) );
+  m3[31] = vabdl_s16( vget_high_s16( m2[14] ), vget_high_s16( m2[15] ) );
+
+  int32x4_t max8 = vmaxq_s32( m3[16], m3[18] );
+  int32x4_t max9 = vmaxq_s32( m3[17], m3[19] );
+  int32x4_t max10 = vmaxq_s32( m3[20], m3[22] );
+  int32x4_t max11 = vmaxq_s32( m3[21], m3[23] );
+  int32x4_t max12 = vmaxq_s32( m3[24], m3[26] );
+  int32x4_t max13 = vmaxq_s32( m3[25], m3[27] );
+  int32x4_t max14 = vmaxq_s32( m3[28], m3[30] );
+  int32x4_t max15 = vmaxq_s32( m3[29], m3[31] );
+
+  const int32x4_t sum0 = horizontal_add_4d_s32x4( max0, max1, max2, max3 );
+  const int32x4_t sum1 = horizontal_add_4d_s32x4( max4, max5, max6, max7 );
+  const int32x4_t sum2 = horizontal_add_4d_s32x4( max8, max9, max10, max11 );
+  const int32x4_t sum3 = horizontal_add_4d_s32x4( max12, max13, max14, max15 );
+
+  const int32x4_t sum0123 = horizontal_add_4d_s32x4( sum0, sum1, sum2, sum3 );
+  uint32_t sad = ( uint32_t )horizontal_add_s32x4( sum0123 );
+  sad <<= 1; // Apply the deferred doubling from the last butterfly.
+  sad -= absDC;
+  sad += absDC >> 2;
+  sad = ( uint32_t )( ( ( double )sad / sqrt( 16.0 * 8.0 ) ) * 2.0 );
+
+  return sad;
+}
+
 Distortion xGetHADs_generic_sve2( const DistParam& rcDtParam )
 {
   const Pel* piOrg = rcDtParam.org.buf;
@@ -525,7 +727,7 @@ Distortion xGetHADs_generic_sve2( const DistParam& rcDtParam )
     {
       for( int x = 0; x < iCols; x += 16 )
       {
-        uiSum += xCalcHAD16x8_neon( &piOrg[x], &piCur[x], iStrideOrg, iStrideCur );
+        uiSum += xCalcHAD16x8_sve2( &piOrg[x], &piCur[x], iStrideOrg, iStrideCur );
       }
       piOrg += 8 * iStrideOrg;
       piCur += 8 * iStrideCur;
@@ -628,7 +830,7 @@ Distortion xGetHADs_sve2( const DistParam& rcDtParam )
     {
       for( int x = 0; x < iCols; x += 16 )
       {
-        uiSum += xCalcHAD16x8_neon( &piOrg[x], &piCur[x], iStrideOrg, iStrideCur );
+        uiSum += xCalcHAD16x8_sve2( &piOrg[x], &piCur[x], iStrideOrg, iStrideCur );
       }
       piOrg += 8 * iStrideOrg;
       piCur += 8 * iStrideCur;
